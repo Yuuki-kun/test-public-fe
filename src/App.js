03e9aa3c821be02a83389a59d913e3c158036528
Yuuki-kun <tongcongminhart@gmail.com>
2023-12-27 21:24:39 +0700

    add new a

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
 import "./App.css";
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  Outlet,
-  BrowserRouter,
-} from "react-router-dom";
+import { Routes, Route, Outlet, BrowserRouter } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import "slick-carousel/slick/slick.css";
